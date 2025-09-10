bb3f2a54fee2794d70fd4cae765768fe70327466
Shaina <shainaz@udel.edu>
2025-09-10 10:42:59 -0400

    working on designs for the pages

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -23,51 +23,14 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <h1>CISC474 Project Starter</h1>
+                <h1>Courses</h1>
+                <hr></hr>
                 <ol>
-                    <li>
-                        Get started by editing{" "}
-                        <code>apps/web/app/page.tsx</code>
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
                 <nav>
                     <Link href="/course">Course</Link>
                 </nav>
-                <Button appName="web" className={styles.secondary}>
-                    Open alert
-                </Button>
+                </ol>
             </main>
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    href="https://turborepo.com?utm_source=create-turbo"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to turborepo.com →
-                </a>
-            </footer>
         </div>
     );
 }
