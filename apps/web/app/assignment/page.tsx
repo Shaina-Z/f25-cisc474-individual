e0c1fe26e1d8808bb0245b68b43dddbef64a14a9
Shaina <shainaz@udel.edu>
2025-09-10 09:43:35 -0400

    added necessary functions to be recognized as pages

---
+++ apps/web/app/assignment/page.tsx
@@ -0,0 +1,5 @@
+export default function Assignment(){
+    return(
+        <>this is going to display an assignment</>
+    )
+}
---
+++ apps/web/app/course/page.tsx
@@ -0,0 +1,5 @@
+export default function Course(){
+    return(
+        <>this will be the courses page</>
+    )
+}
