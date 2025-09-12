44aadbdd380966b1bce7c1a92e2c376aeb62c17d
Shaina <shainaz@udel.edu>
2025-09-12 18:25:26 -0400

    added a back button for profile page

--- apps/web/app/profile/page.tsx
+++ apps/web/app/profile/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import "./page.css";
 
 export default function Profile(){
     return(
         <div>
+            <Link className="back-button" href="./course-view">Back</Link>
             <div className="profile-image"></div>
         </div>
     )
