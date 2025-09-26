2a0bafcfe0513f34417f035ec6615a0bba2f3854
Shaina <shainaz@udel.edu>
2025-09-26 13:29:44 -0400

    added code to service to find all users

--- apps/api/src/user.service.ts
+++ apps/api/src/user.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { User, Prisma} from '@repo/database';
 
 @Injectable()
 export class UserService {
-  
+   constructor(private prisma: PrismaService) {}
+
+    async users(params: {
+     skip?: number;
+    take?: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }):Promise<User[]>{
+    const{skip, take, cursor, where, orderBy } = params
+    return this.prisma.user.findMany({skip, take, cursor, where, orderBy })
+  }
+
 }
