432bd7835fd38e747df8aaa50b11a6dcab5b3147
Shaina <shainaz@udel.edu>
2025-09-26 15:48:36 -0400

    added module that connects the user service and controller, also imported it to the app module

--- apps/api/src/user.controller.ts
+++ apps/api/src/user.controller.ts
@@ -1,2 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
+import {User as UserModel} from '@repo/database';
 import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController{
+    constructor(
+        private readonly userService: UserService){}
+    
+    @Get()
+    async findAll():(Promise<UserModel[]>){
+        return this.userService.users({})
+    }
+
+
+}
+
