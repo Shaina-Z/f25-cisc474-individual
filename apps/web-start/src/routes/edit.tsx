d9ceba1f6d40d9433b18ecf757b1102eed637704
Shaina <shainaz@udel.edu>
2025-10-20 21:31:16 -0400

    starting to work on mutations for DTOs

--- apps/web-start/src/routes/edit.tsx
+++ apps/web-start/src/routes/edit.tsx
@@ -1,15 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useState } from 'react';
 
 export const Route = createFileRoute('/edit')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [newTitle,setNewTitle] = useState('');
   return <div>
     <h1>Edit/Add</h1>
     <input
     type="text"
     placeholder='Enter Course Name'
+    value={newTitle}
+    onChange={(e)=> setNewTitle(e.target.value)}
     ></input>
+    <button > create course</button>
   </div>;
 }
