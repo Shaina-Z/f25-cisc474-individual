3edbc91e6a2bc9a62c2af8a196580e4faa5a14d8
Shaina <shainaz@udel.edu>
2025-10-22 20:39:50 -0400

    added some auth guards to course endpoints

--- apps/api/src/course.controller.ts
+++ apps/api/src/course.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Get, Param, Post} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post,UseGuards} from '@nestjs/common';
 import {Course as CourseModel} from '@repo/database';
 import { CourseService } from './course.service';
 import { CourseIn } from '@repo/api/courses';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from './current-user.decorator';
+import { JwtUser } from './jwt.strategy';
 
 @Controller('course')
 export class CourseController{
     constructor(
         private readonly courseService: CourseService){}
+
+    @UseGuards(AuthGuard('jwt'))
     @Get('/:id')
         async getUserById(@Param('id') id: string): Promise<CourseModel> {
         return this.courseService.findCourse({ id: Number(id) });
@@ -17,6 +22,7 @@ export class CourseController{
         return this.courseService.findCourses({})
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Post()
     create(@Body() createCourseDto: CourseIn) {
     return this.courseService.create(createCourseDto);
