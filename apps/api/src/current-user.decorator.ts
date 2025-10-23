718416cf5b72ac8227c459b605cbfeb4359e6428
Shaina <shainaz@udel.edu>
2025-10-22 22:47:31 -0400

    working on auth

--- apps/api/src/current-user.decorator.ts
+++ apps/api/src/current-user.decorator.ts
@@ -1,9 +1,9 @@
-// import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-// import { JwtUser } from './jwt.strategy';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { JwtUser } from './jwt.strategy';
 
-// export const CurrentUser = createParamDecorator(
-//   (data: unknown, ctx: ExecutionContext): JwtUser => {
-//     const req = ctx.switchToHttp().getRequest();
-//     return req.user as JwtUser;
-//   },
-// );
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): JwtUser => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.user as JwtUser;
+  },
+);
